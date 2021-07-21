3e10a9487057e73e957873866d88cccc76227f56
Bhupendra <bhupendrasam1404@gmail.com>
2021-07-22 03:33:10 +0530

    PHP develelopmanet started

---
+++ php/products.php
@@ -0,0 +1,31 @@
+<div class="container tag-yellow">
+        <div class="box-title bg-light m-2">
+          Out Products&ensp;&ensp;&ensp;&ensp; 📱
+        </div>
+      </div>
+      <section class="container">
+
+        <ul>
+          <li class="product-list active" data-filter="all">ALL</li>
+          <li class="product-list" data-filter="redmi">Redmi MI</li>
+          <li class="product-list" data-filter="apple">Apple</li>
+          <li class="product-list" data-filter="samsung">Samsung</li>
+        </ul>
+        <div class="product-filter">
+          <a href="product.php"><div class="itembox samsung"><img src="./images/products/1.png" alt=""><br><div class="product-name-filter">Product</div></div></a>
+          <a href="product.php"><div class="itembox redmi"><img src="./images/products/2.png" alt=""><br><div class="product-name-filter">Product</div></div></a>
+          <a href="product.php"><div class="itembox redmi"><img src="./images/products/2.png" alt=""><br><div class="product-name-filter">Product</div></div></a>
+          <a href="product.php"><div class="itembox redmi"><img src="./images/products/3.png" alt=""><br><div class="product-name-filter">Product</div></div></a>
+          <a href="product.php"><div class="itembox redmi"><img src="./images/products/4.png" alt=""><br><div class="product-name-filter">Product</div></div></a>
+          <a href="product.php"><div class="itembox redmi"><img src="./images/products/5.png" alt=""><br><div class="product-name-filter">Product</div></div></a>
+          <a href="product.php"><div class="itembox redmi"><img src="./images/products/6.png" alt=""><br><div class="product-name-filter">Product</div></div></a>
+          <a href="product.php"><div class="itembox redmi"><img src="./images/products/8.png" alt=""><br><div class="product-name-filter">Product</div></div></a>
+          <a href="product.php"><div class="itembox samsung"><img src="./images/products/10.png" alt=""><br><div class="product-name-filter">Product</div></div></a>
+          <a href="product.php"><div class="itembox samsung"><img src="./images/products/11.png" alt=""><br><div class="product-name-filter">Product</div></div></a>
+          <a href="product.php"><div class="itembox samsung"><img src="./images/products/12.png" alt=""><br><div class="product-name-filter">Product</div></div></a>
+          <a href="product.php"><div class="itembox apple"><img src="./images/products/13.png" alt=""><br><div class="product-name-filter">Product</div></div></a>
+          <a href="product.php"><div class="itembox apple"><img src="./images/products/14.png" alt=""><br><div class="product-name-filter">Product</div></div></a>
+          <a href="product.php"><div class="itembox apple"><img src="./images/products/15.png" alt=""><br><div class="product-name-filter">Product</div></div></a>
+
+        </div>
+      </section>
